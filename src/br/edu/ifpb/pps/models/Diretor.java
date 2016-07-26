0f8087378c3b5b8f8f72daf2af2c831623aa2f2c
matheusmayer <matheusmayerduarte@gmail.com>
2016-07-25 23:50:43 -0300

    [UPDATE] - Impementando o padrao singleton na classe diretor

--- src/br/edu/ifpb/pps/models/Diretor.java
+++ src/br/edu/ifpb/pps/models/Diretor.java
@@ -1,5 +1,32 @@
 package br.edu.ifpb.pps.models;
 
-public class Diretor {
+/**
+ * Esta classe fará com que seja instaciado apenas um diretor.
+ * @author Matheus Mayer
+ * @since 25/07/2016
+ */
+public class Diretor 
+{
+	private static Diretor unicoDiretor;
 
+	/**
+	 * Este metodo será privado para que não haja mais de uma instancia.
+	 * @author Matheus Mayer
+	 * @since 25/07/2016
+	 */
+	private Diretor(){}
+	
+	/**
+	 * @author Matheus Mayer
+	 * @since 25/07/2016
+	 * @return Diretor unicoDiretor
+	 */
+	public static synchronized Diretor getInstance()
+	{
+		if (unicoDiretor == null) {
+			unicoDiretor = new Diretor(){};
+		}
+		
+		return unicoDiretor;
+	}
 }
