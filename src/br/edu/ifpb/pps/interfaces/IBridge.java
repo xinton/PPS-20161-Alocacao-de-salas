f25fed4a4eeb632aa14db6cdfcedd7037659a132
Pedro Paiva <pedrovsp109@gmail.com>
2016-07-26 19:56:54 -0300

    Esqueci a interface, eita

---
+++ src/br/edu/ifpb/pps/interfaces/IBridge.java
@@ -0,0 +1,5 @@
+package br.edu.ifpb.pps.interfaces;
+
+public interface IBridge {
+	public void funcao(String tipo, String apelido);
+}
---
+++ src/br/edu/ifpb/pps/interfaces/IBuilderEvento.java
@@ -0,0 +1,21 @@
+package br.edu.ifpb.pps.interfaces;
+
+import java.util.Date;
+
+import br.edu.ifpb.pps.models.Evento;
+
+/**
+ * @author Matheus Mayer
+ * @since 26/07/2016
+ */
+public interface IBuilderEvento 
+{
+	public Evento getEvento();
+	public void criarNovoEvento();
+	public void definirNome(String nome);
+	public void definirDataInicio(Date dataIni);
+	public void definirDataFim(Date dataFim);
+	public void definirContato(String contato);
+	public void definirRepeticoes(int repeticoes);
+	public void definirRepete(boolean repete); // TROCAR ESTE NOME PARA IS!
+}
