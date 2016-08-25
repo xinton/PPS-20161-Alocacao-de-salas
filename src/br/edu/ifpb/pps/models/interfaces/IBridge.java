76fa77e5ac663d7061867cca01c25d1d98dd602f
mayermatheus <matheusmayerduarte@gmail.com>
2016-08-25 20:21:53 -0300

    [ADD] - INTEGRANDO AS INTERFACES QUE DIEGO ME REPASSOU

--- src/br/edu/ifpb/pps/models/interfaces/IBridge.java
+++ src/br/edu/ifpb/pps/models/interfaces/IBridge.java
@@ -1,5 +1,6 @@
 package br.edu.ifpb.pps.models.interfaces;
 
 public interface IBridge {
-	public void funcao(String tipo, String apelido);
+	public void funcao(int tipo, String apelido);
+	public String getTipo();
 }
