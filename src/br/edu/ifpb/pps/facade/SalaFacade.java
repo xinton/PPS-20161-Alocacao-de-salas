68a1c1c93abda7201d302763423f3ee48bc8e766
mayermatheus <matheusmayerduarte@gmail.com>
2016-08-30 22:07:27 -0300

    Implementando as classes facade

--- src/br/edu/ifpb/pps/facade/SalaFacade.java
+++ src/br/edu/ifpb/pps/facade/SalaFacade.java
@@ -1,5 +1,67 @@
 package br.edu.ifpb.pps.facade;
 
-public class SalaFacade {
+import java.util.ArrayList;
 
+import br.edu.ifpb.pps.models.Aula;
+import br.edu.ifpb.pps.models.Conferencia;
+import br.edu.ifpb.pps.models.Laboratorio;
+import br.edu.ifpb.pps.models.Sala;
+import br.edu.ifpb.pps.models.VideoConferencia;
+import br.edu.ifpb.pps.models.interfaces.IBridge;
+import br.edu.ifpb.pps.models.interfaces.TipoSala;
+
+/*
+ * Esta classe tem o objetivo de simular a base de dados e 
+ * ser responsável pelas ações da sala.
+ * @author Matheus Mayer <matheusmayerduarte@gmail.com>
+ */
+public class SalaFacade
+{
+	private static ArrayList<Sala> salas = new ArrayList<Sala>();
+	
+	public SalaFacade(){}
+	// ALGUÉM PEGA ESSE METODO!!
+	public void adicionarSala(String id, String apelido, int capacidade, int IDsala){
+		IBridge sala;	
+		TipoSala tipoSala;	
+		
+		if (IDsala == 1) {
+			sala = new Aula();
+		} else if(IDsala == 2) {
+			sala = new Laboratorio();
+		} else if(IDsala == 3) {
+			sala = new Conferencia(); 
+		} else {
+			sala = new VideoConferencia();
+		}
+		salas.add(new Sala(id, apelido, capacidade, sala));
+		IDsala += 1;
+	}
+	
+	public void removerSala(String idSala){
+		Sala sala = getSalaId(idSala);
+		salas.remove(sala);
+	}
+	
+	/*
+	 * Este metodo servirá para resgatar a primeira sala disponivel
+	 * @author Matheus Mayer <matheusmayerduarte@gmail.com> 
+	 */
+	public Sala procurarSalaDisponivel(){
+		for (Sala sala : salas) {
+			if (sala.getEvento() == null) {
+				return sala;
+			}
+		}
+		return null;
+	}
+	/*
+	 * @author Diego Carvalho
+	 */
+	public Sala getSalaId(String id){
+		for(Sala sala: salas)
+			if(sala.getApelido().equals(id))
+				return sala;
+		return null;
+	}
 }
